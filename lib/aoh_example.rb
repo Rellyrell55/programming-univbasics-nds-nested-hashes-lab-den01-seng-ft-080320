2674edc0324d1bc27e4fcebc4235c9ae0ff0567f
Terrell Cooper <terrellcoop@gmail.com>
2020-07-23 19:12:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,8 +14,11 @@ end
 def literal_aoh
  names = [
    { name:  "Don Gately", occupation: "Live-in Staff" },
-   { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-   { name:  "Pat Monteseian", occupation: "Staff" }
+   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+   { name:  "Pat Monteseian", occupation: "Staff" },
+   { name:  "Kate Gompert", occupation: "None" },
+   { name:  "Bruce Green", occupation: "Fan of Mildred" }
+   ]
 end
 
 def aoh_lookup(aoh, row, key)
